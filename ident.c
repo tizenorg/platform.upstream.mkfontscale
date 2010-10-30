64b1f54bcebc6ed628f74f0204d9ec7242e6e9ce
Alan Coopersmith <alan.coopersmith@oracle.com>
2010-10-29 22:57:04 -0700

    Sun's copyrights now belong to Oracle
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- ident.c
+++ ident.c
@@ -20,7 +20,7 @@
   THE SOFTWARE.
 */
 /*
- * Copyright © 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
